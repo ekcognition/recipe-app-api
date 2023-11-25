4117afbd9b74726631a013dd33120292db2f7523
Eugenio Kloster <eugeniokloster@Eugenios-MBP.fibertel.com.ar>
2023-11-25 17:40:19 -0300

    adding new test cases

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x , y):
+    """ Add x and y and return result."""
+    return x + y
+
+def substract(x, y):
+    """ Substract 2 numbers and return result"""
+    return x - y
